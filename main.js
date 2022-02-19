e23e9c4897debc3ce20ba420ae8090a103ed5dfc
mpatzem <mpatzem@gmail.com>
2022-02-19 15:50:00 -0700

    2.19.22 Fixes
    1. Swapped class with id for carousel right wrapper
    2. added additional javascript to flip images when slider button is clicked, giving illusion of the slider moving to that right-wrapper position
    3. added blue overlay background above background img in carousel right wrapper
    4. added comments

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
+// Sets value for slide position/counter
 let slidePosition = 0;
+// Grabs slides
 const slides = document.getElementsByClassName('carousel__item');
 const totalSlides = slides.length;
-
+// Next button feature
 document.
   getElementById('carousel__button--next')
   .addEventListener("click", function() {
     moveToNextSlide();
+    changeImageBackground();
   });
+// Previous button feature
 document.
   getElementById('carousel__button--prev')
   .addEventListener("click", function() {
     moveToPrevSlide();
+    changeImageBackground();
   });
-
+// Upates slider position - toggles visibility of previous slide
 function updateSlidePosition() {
   for (let slide of slides) {
     slide.classList.remove('carousel__item--visible');
@@ -21,7 +26,7 @@ function updateSlidePosition() {
 
   slides[slidePosition].classList.add('carousel__item--visible');
 }
-
+// Counter to move to next slide
 function moveToNextSlide() {
   if (slidePosition === totalSlides - 1) {
     slidePosition = 0;
@@ -31,7 +36,7 @@ function moveToNextSlide() {
 
   updateSlidePosition();
 }
-
+// Counter to move to previous slide
 function moveToPrevSlide() {
   if (slidePosition === 0) {
     slidePosition = totalSlides - 1;
@@ -40,4 +45,9 @@ function moveToPrevSlide() {
   }
 
   updateSlidePosition();
+}
+// Function to swap imgs on right side of slider
+function changeImageBackground() {
+    var imageBack = document.getElementById('carousel-right-wrapper');
+    imageBack.classList.toggle('lastImg');
 }
